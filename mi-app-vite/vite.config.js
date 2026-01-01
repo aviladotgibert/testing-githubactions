ef4e84786df554163b0939e71ceaa8c239e4e3f3
aviladotgibert <a.viladotgibert@gmail.com>
2026-01-01 18:38:28 +0100

    Update vite.config.js

--- mi-app-vite/vite.config.js
+++ mi-app-vite/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/testing-githubactions/mi-app-vite/', // nombre EXACTO del repo
+  base: '/testing-githubactions/', // nombre EXACTO del repo
 })
